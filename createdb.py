a4858985ff0ecb61946903afc7085d86e516d709
Vignesh Mohankumar <vignesh@vigneshmohankumar.com>
2014-12-24 18:08:49 -0500

    Initial query file and move utils, models into createdb.

--- createdb.py
+++ createdb.py
@@ -1,12 +1,101 @@
 import json
+import os
 import pickle
+import requests
+import string
 
+from bs4 import BeautifulSoup
 from datetime import datetime
 from itertools import combinations, izip
+from mongokit import Connection
+from posixpath import basename
+from urlparse import urlparse
 
-from database import connection
-from utils import (find_player_code, get_header, is_number,
-                   path_components_of_url, soup_from_url)
+connection = Connection()
+
+
+class Gamelog(Document):
+    __collection__ = 'gamelogs'
+    structure = {
+        'FT': float,
+        'TP': float,
+        'TOV': float,
+        'Tm': str,
+        'GmSc': str,
+        'FG': float,
+        'TPA': float,
+        'DRB': float,
+        'Rk': float,
+        'Opp': str,
+        'AST': float,
+        'Season': str,
+        'HomeAway': str,
+        'Date': datetime,
+        'PF': float,
+        'WinLoss': float,
+        'FGA': float,
+        'GS': float,
+        'G': float,
+        'STL': float,
+        'Age': str,
+        'TRB': float,
+        'FTA': float,
+        'BLK': float,
+        'PlusMinus': float,
+        'PTS': float,
+        'Player': str,
+        'MP': float,
+        'Year': int,
+        'ORB': float
+    }
+    required_fields = ['Opp', 'G', 'Season', 'Age', 'HomeAway', 'Player',
+                       'Tm', 'Year', 'Date', 'GS', 'WinLoss', 'Rk']
+    use_dot_notation = True
+
+
+class Headtohead(Document):
+    __collection__ = 'headtoheads'
+    structure = {
+        'Player': str,
+        'Opp_Player': str,
+        'FT': float,
+        'TP': float,
+        'TOV': float,
+        'Tm': str,
+        'FG': float,
+        'TPA': float,
+        'DRB': float,
+        'Rk': float,
+        'Opp': str,
+        'AST': float,
+        'Season': str,
+        'HomeAway': str,
+        'Date': datetime,
+        'PF': float,
+        'WinLoss': float,
+        'FGA': float,
+        'GS': float,
+        'STL': float,
+        'TRB': float,
+        'FTA': float,
+        'BLK': float,
+        'PTS': float,
+        'MP': float,
+        'ORB': float
+    }
+    required_fields = ['Player', 'Opp_Player', 'Opp', 'Season', 'HomeAway',
+                       'Tm', 'Date', 'GS', 'WinLoss', 'Rk']
+    use_dot_notation = True
+
+
+class Salary(Document):
+    __collection__ = 'salaries'
+    structure = {
+        'Player': str,
+        'Salary': dict
+    }
+    required_fields = ['Player', 'Salary']
+    use_dot_notation = True
 
 
 def find_gamelogs_from_url(
@@ -294,6 +383,7 @@ def add_gamelogs_in_table_to_db(
                 gamelog['Opp_Player'] = gamelog.pop('player_code_1')
 
         # Initializes connection to the correct collection in database.
+
         collection = connection[collection_id].users
         collection.insert(gamelog) # Inserts the gamelog dictionary to db.
         print collection.find_one(gamelog) # Finds dict to ensure addition.
@@ -349,7 +439,6 @@ def create_headtoheads_collection():
     return 'ALL HEADTOHEADS ADDED.'
 
 
-'''
 def create_salaries_collection():
     """
     Adds all player salaries to the database. In the salaries table, each row
@@ -364,4 +453,183 @@ def create_salaries_collection():
         'table', attrs={'id': 'contracts'})
 
     print table
-'''
+
+
+def get_header(table):
+    """
+    Finds and returns the header of a table.
+
+    Args:
+        table: HTML table of gamelog stats for one year. Can be a regular
+            season or playoff table, or regular gamelog or headtohead gamelog
+            table.
+
+    Returns:
+        list of str if table, None otherwise.
+
+    """
+    header = []
+    try:
+        for th in table.findAll('th'):
+            col = str(th.getText())
+            col = col.replace('%', 'P') \
+                     .replace('3', 'T') \
+                     .replace('+/-', 'PlusMinus')
+
+            if col not in header:
+                header.append(col)
+    except AttributeError:
+        return None
+
+    return header
+
+
+def is_number(s):
+    """
+    Checks if a string is a number.
+
+    Args:
+        s (str): a string containing a number.
+
+    Returns:
+        bool: True if a string is a number, false otherwise.
+
+    Raises:
+        NotImplementedError: If s is not a string.
+
+    """
+    if isinstance(s, str):
+        try:
+            float(s)
+            return True
+        except ValueError:
+            return False
+    else:
+        raise NotImplementedError('Must enter a string.')
+
+
+def soup_from_url(url, payload=None):
+    """
+    Uses BeautifulSoup to scrape a website to get its soup.
+
+    Args:
+        url (string): Basketball-Reference url consisting of gamelogs for a
+            single year of player stats.
+        payload (dict, optional): Payload for a Requests url request. In this
+            case, only headtohead_url requires a payload, which contains the
+            keys p1, p2 and request.
+
+    Returns:
+        BeautifulSoup formatted HTML of scraped url.
+
+
+    """
+    try:
+        if payload:
+            r = requests.get(url, params=payload)
+        else:
+            r = requests.get(url)
+        return BeautifulSoup(r.text)
+    except:
+        return None
+
+
+def find_player_code(player):
+    """
+    Finds a player code given a player name.
+
+    Args:
+        player (str): Name of a player to look up in player_names_urls.
+
+    Returns:
+        str: player_code of player if successful.
+        None: if player lookup raises KeyError.
+
+    Todo:
+        Use MongoDB as a semi-intelligent cache, instatiating players as
+            items, inserting and deleting players as needed.
+
+    """
+    with open('./player_names_urls.json') as f:
+        player_names_urls = json.load(f)
+
+    try:
+        player_url = player_names_urls[player]
+    except KeyError:
+        return None
+
+    player_url_path = urlparse(player_url).path
+    bn = basename(player_url_path)
+    player_code = os.path.splitext(bn)[0]
+
+    return player_code
+
+
+def path_components_of_url(url):
+    """
+    Splits a url and returns a list of components of the url's path.
+
+    """
+    o = urlparse(url)
+    path_components = o.path.split('/')
+    return path_components
+
+
+def save_player_names_urls():
+    """
+    Saves a dictionary of player names and basketball-reference home urls.
+
+    Finds the directory of players with last name starting with a specific
+    letter for every lowercase letter. Current names have strong tags, so
+    finds all current_names.
+
+    """
+    names = []
+    for letter in string.ascii_lowercase:
+        letter_page = soup_from_url(
+            'http://www.basketball-reference.com/players/%s/' % (letter))
+
+        current_names = letter_page.findAll('strong')
+        for n in current_names:
+            name_data = n.children.next()
+            names.append(
+                (name_data.contents[0],
+                 'http://www.basketball-reference.com' +
+                    name_data.attrs['href']))
+
+    with open('./player_names_and_urls.json', 'w') as f:
+        json.dump(dict(names), f)
+
+    return "PLAYER NAMES AND URLS SAVED."
+
+
+def save_gamelog_urls():
+    """
+    Returns list of gamelog urls with every year of every current player.
+
+    Opens player_names_urls, a JSON file containing a list of dictionaries
+    with key values Name and URL. For each dictionary in the list, url is
+    scraped and the table containing the player totals is stored. Finds each
+    single season table in the totals table. In each single season table, the
+    gamelog url found by searching for url column and finding the link text.
+    """
+
+    player_names_urls = open_player_names_urls()
+
+    gamelog_urls = []
+    for name, url in player_names_urls.items():
+        table_soup = soup_from_url(url)
+
+        totals_table = table_soup.find('table', attrs={'id': 'totals'})
+        all_tables = totals_table.findAll('tr', attrs={'class': 'full_table'})
+
+        for table in all_tables:
+            url = table.find('td')
+            for link in url.findAll("a"):
+                gamelog_urls.append('http://www.basketball-reference.com' +
+                                        link.get("href"))
+
+    with open('./gamelog_urls', 'wb') as f:
+        pickle.dump(gamelog_urls, f)
+
+    return 'GAMELOG URLS SAVED.'
---
+++ query.py
@@ -0,0 +1,144 @@
+import arrow
+import bson.json_util as bson
+import numpy
+
+from mongokit import Connection
+from sortedcontainers import SortedListWithKey, SortedList
+
+from createdb import find_player_code
+
+connection = Connection()
+
+
+class Calculate(object):
+    """
+    example queries:
+        my_query = {'PTS': {'$gt': 30}}
+        my_query = dict(PTS=81)
+        my_query = QueryHelpers(-100).datetime_range()
+
+    calculate = Calculate(my_query)
+
+    calculate.top_dfs_scores(15)
+    calculate.print_gamelogs()
+    calculate.find_stats('PTS')
+
+    """
+
+    def __init__(self, query, site):
+        self.query = query
+        self.site = site
+        self.gamelogs = connection.gamelogs.users.find(self.query)
+
+    def print_gamelogs(self):
+        for gamelog in self.gamelogs:
+            print gamelog
+        return
+
+    def find_stats(self, stat):
+        if connection.gamelogs.users.find_one(self.query):
+            return [
+                gamelog[stat]
+                for gamelog in self.remove_dnps(self.gamelogs)
+            ]
+        else:
+            return None
+
+    def remove_dnps(self, gamelogs):
+        if gamelogs:
+            return [
+                gamelog
+                for gamelog in gamelogs
+                if (gamelog['GS'] != 'Inactive'
+                    and
+                    gamelog['GS'] != 'Did Not Play'
+                    and
+                    gamelog['GS'] != 'Player Suspended')
+            ]
+        else:
+            return None
+
+    def stat_avg(self, stat):
+        stats = self.find_stats(stat)
+        if stats:
+            return numpy.mean(self.find_stats(stat))
+        else:
+            return None
+
+    def is_double_triple_double(self, stats):
+        if stats[1] >= 10:
+            if stats[2] >= 10:
+                return 3.00 # Triple Double
+            else:
+                return 1.50 # Double Double
+        else:
+            return 0 # Neither
+
+    def create_dfs_scores_sorted_list(self):
+        dfs_scores = [
+            dict(score=self.calc_dfs_score_from_gamelog(gamelog),
+                 gamelog=gamelog)
+            for gamelog in self.remove_dnps(self.gamelogs)
+        ]
+
+        return SortedListWithKey(dfs_scores, key=lambda val: -val['score'])
+
+    def top_dfs_scores(self, number):
+        dfs_scores = self.create_dfs_scores_sorted_list()
+        for num, score in enumerate(dfs_scores[0:(number)]):
+            print (
+                str(num+1) +
+                ') ' +
+                str(score['gamelog']['Player']) +
+                ' vs. ' +
+                str(score['gamelog']['Opp']) +
+                ' on ' +
+                str(score['gamelog']['Date'].strftime("%m/%d/%Y")) +
+                ' -- ' +
+                str(score['score']))
+            print bson.dumps(score['gamelog'], indent=4)
+            print
+
+    def calc_dfs_score_from_gamelog(self, gamelog):
+        TP  = gamelog['TP']
+        AST = gamelog['AST']
+        TRB = gamelog['TRB']
+        STL = gamelog['STL']
+        BLK = gamelog['BLK']
+        TOV = gamelog['TOV']
+
+        if self.site == 'DraftKings':
+            PTS = gamelog['PTS']
+
+            sorted_stats = SortedListWithKey(
+                list(PTS, AST, TRB, STL, BLK),
+                key=lambda val: -val)
+            DDorTD = self.check_double_triple_double(sorted_stats)
+
+            score = (PTS * 1.00 + TP  * 0.50 + AST * 1.50 + TRB * 1.25 +
+                     STL * 2.00 + BLK * 2.00 - TOV * 0.50 + DDorTD)
+
+        elif self.site == 'Fanduel':
+            FG  = gamelog['FG'] - TP
+            FT  = gamelog['FT']
+
+            score = (FG  * 2.00 + TP  * 3.00 + FT  * 1.00 + AST * 1.50 +
+                     TRB * 1.20 + STL * 2.00 + BLK * 2.00 - TOV * 1.00)
+
+        return score
+
+
+class QueryHelpers(object):
+    def __init__(self, start=None, end=None):
+        self.days_range = days_range
+
+    def datetime_range(self):
+        start = arrow.utcnow().replace(days=self.days_range).datetime
+        end = arrow.utcnow().datetime
+        return {'Date': {'$gte': start, '$lt': end}}
+
+
+if __name__ == '__main__':
+    my_query = QueryHelpers(-365, -).datetime_range()
+    calculate1 = Calculate(my_query, "DraftKings")
+    calculate1.top_dfs_scores(5)
