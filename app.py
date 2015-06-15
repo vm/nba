0d8bdc8bf969c2dc0b7e30ebc875513c36a9460c
Vignesh Mohankumar <vignesh@vigneshmohankumar.com>
2015-06-14 23:35:02 -0400

    Remove unused framework files.

---
+++ app.py
@@ -0,0 +1,5 @@
+from pymongo import MongoClient
+
+connection = MongoClient()
+db = connection.nba
+
--- main.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from nba.app import app
-from nba import routes
-
-
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
